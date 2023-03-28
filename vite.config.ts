3fd3e9642645b4b15808edf459d08c4e61b5a3b8
Marcel Suter <github@marcelsuter.ch>
2023-03-28 06:21:28 +0000

    added app mode config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+
+  /* path /vue-audio-level-mete/ is suitable for publishing to github pages */
+  base: "/vue-audio-level-meter/",
+
+  build: {
+    /* Defining the options for "App Mode" */
+    rollupOptions: {
+      external: [/^.*(.mp3)/, /^.*(.ogg)/],
+    },
+
+    manifest: true,
+    sourcemap: true,
+  },
+});
